cf355dbd289a4ebec8bb8d581cc2de0fb11cf2a5
Manaswi-01 <manaswikabadi@gmail.com>
2026-02-19 18:28:37 +0530

    Hostel section done

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {
     getStudents,
-    getAnalytics
+    getAnalytics,
+    getHostelApplications,
+    approveRejectHostel
 } = require('../controllers/adminController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 router.route('/students').get(protect, adminOnly, getStudents);
 router.route('/analytics').get(protect, adminOnly, getAnalytics);
+router.route('/hostel-applications').get(protect, adminOnly, getHostelApplications);
+router.route('/hostel-applications/:studentId').put(protect, adminOnly, approveRejectHostel);
 
 module.exports = router;
+
